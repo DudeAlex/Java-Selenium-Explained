191d7171a01c3f9133886797dd746cb08e2fbc5c
Alex <81188188+DudeAlex@users.noreply.github.com>
2023-05-09 13:27:15 -0700

    Added StringFormatter to MyUtilTest (#2)

--- src/test/java/runner/MyUtils.java
+++ src/test/java/runner/MyUtils.java
@@ -6,15 +6,20 @@ import org.openqa.selenium.TakesScreenshot;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MyUtils {
     public static <T> void takeScreenshot(T driver, String methodName, String className) throws IOException {
         Date currentDate = new Date();
-        String cdate = currentDate.toString().replace(" ", "-").replace(":","-");
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd-hh-mm-ss");
+        String formattedDate = formatter.format(currentDate);
+
         TakesScreenshot screenshot = (TakesScreenshot) driver;
         File file = screenshot.getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(file, new File(String
-                .format(".\\src\\test\\java\\screenshots\\screenshot-%s-%s-%s.png",methodName, className, cdate)));
+                .format(".\\src\\test\\java\\screenshots\\%s-%s-%s.png",
+                        methodName, className, formattedDate)));
     }
 }
